515b46e2b3fe26ad7549dafa3782fa2c6317e5e7
thom <thomwilhelm@gmail.com>
2014-05-07 10:32:21 +1000

    Output the news and events items as blocks coming from views

--- sites/all/themes/msd2014/templates/node--front.tpl.php
+++ sites/all/themes/msd2014/templates/node--front.tpl.php
@@ -15,68 +15,21 @@ print drupal_render($build);
 </div>
 
 <div class="right-column-wrap">
+
 <div class="news-wrap">
-<h2>MSD Latest News</h2>
-
-<ul>
-<li>
-<a href="#"><span>Wed, 20 Feb 2014</span>
-Lorem ipsum dolor sit ipsum <br>
-amet consectetur adipisicing more</a></li>
-
-<li>
-<a href="#"><span>Fri, 2 Mar 2014</span>
-Lorem ipsum dolor sit amet<br>
-consectetur adipisicing more</a></li>
-
-<li>
-<a href="#"><span>Wed, 20 Feb 2014</span>
-Lorem ipsum dolor sit amet ipsum<br>
-consec tetur adipisicing more</a></li></ul>
-
+<?php
+$block = block_load('views', 'news-block');
+$build = _block_get_renderable_array(_block_render_blocks(array($block)));
+print drupal_render($build);
+?>
 </div>
 
-
-
-
 <div class="upcoming-events-wrap">
-<h2>Upcoming Events</h2>
-<ul>
-
-<li><a href="#">
-<span class="spleft">
-<span class="nmbr">20</span>
-<span class="date">FEB</span>
-</span>
-<span class="spright">Lorem ipsum dolor sit consectetur adipisicing</span>
-</a></li>
-
-<li><a href="#">
-<span class="spleft">
-<span class="nmbr">05</span>
-<span class="date">MAR</span>
-</span>
-<span class="spright">Lorem ipsum dolor sit consectetur adipisicing</span>
-</a></li>
-
-<li><a href="#">
-<span class="spleft">
-<span class="nmbr">15</span>
-<span class="date">MAR</span>
-</span>
-<span class="spright">Lorem ipsum dolor sit consectetur adipisicing</span>
-</a></li>
-
-<li><a href="#">
-<span class="spleft">
-<span class="nmbr">25</span>
-<span class="date">APR</span>
-</span>
-<span class="spright">Lorem ipsum dolor sit consectetur adipisicing</span>
-</a></li>
-
-</ul>
-
+<?php
+$block = block_load('views', 'events-block');
+$build = _block_get_renderable_array(_block_render_blocks(array($block)));
+print drupal_render($build);
+?>
 </div>
     
   </div>
