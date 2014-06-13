5bb78c835260efc808a4b048cae08bf0a9a738c9
thom <thomwilhelm@gmail.com>
2014-06-13 15:43:21 +1000

    Added template for node type blog post, added field styling for blog tags

---
+++ sites/all/themes/msd2014/templates/node--blog-post.tpl.php
@@ -0,0 +1,59 @@
+<div class="content-white-wrap"><!--white wrap start-->
+
+<div class="content-left-wrap">
+
+<?php 
+$breadcrumb = drupal_get_breadcrumb();
+if ($breadcrumb): ?>
+	<div class="thecrumb">
+		<div id="breadcrumb"><?php print theme('breadcrumb', array('breadcrumb'=>$breadcrumb)); ?></div>
+	</div>
+<?php endif; ?>
+  
+  <?php if ($title): ?>
+      <h1><?php print $title; ?></h1>
+  <?php endif; ?>
+
+  <div>
+  	<?php print format_date($node->created, 'blog_post'); ?>
+  </div>
+  
+  <?php print render($content['body']); ?>
+  
+  <?php print render($content['field_blog_tags']); ?>
+  
+</div>
+
+<div class="content-right-wrap">
+	
+	<?php
+	$block = module_invoke('sharethis_extras', 'block_view', 'sharethis_extras_block');
+	print render($block['content']);
+	?>
+	
+	<?
+	$item = field_get_items('node', $node, 'field_right_column');
+	if(is_array($item)) {
+	
+		$field_right_column = field_view_value('node', $node, 'field_right_column', $item[0]);
+		if($field_right_column && is_array($field_right_column)) { ?>
+			<div class='right-column'>
+				<? print render($field_right_column); ?>
+			</div>
+	    <? } ?>
+    
+    <? } ?>
+    
+	<? if(isset($rightcolumn) && is_array($rightcolumn)) { ?>
+	    <div class='right-column'>
+	    <? print render($rightcolumn); ?>
+	    </div>
+    <? } ?>
+	
+</div>
+<div style="clear:both"></div>
+  
+  <div class="backtop"><a href="#">Back to top</a></div>
+  
+  
+  </div><!--white wrap end-->
