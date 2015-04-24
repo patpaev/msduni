a1a1d7536db17eccfc4f06aa2c5d8989f663ee06
Patrick Paevere <patrick@paevere.com>
2015-04-24 16:17:41 +1000

    all the favicons!

--- sites/all/themes/msd2014/templates/html.tpl.php
+++ sites/all/themes/msd2014/templates/html.tpl.php
@@ -96,6 +96,23 @@ $site_name = variable_get('site_name', '');
 	<meta name="DC.Date" content="DATE">
 	<meta name="DC.Date.Modified" content="DATE">
 <!-- End meta data to be autofilled -->
+
+<!-- favicons -->
+<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+<link rel="icon" type="image/png" href="/favicon-196x196.png" sizes="196x196">
+<link rel="icon" type="image/png" href="/favicon-160x160.png" sizes="160x160">
+<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+<meta name="msapplication-TileColor" content="#00457c">
+<meta name="msapplication-TileImage" content="/mstile-144x144.png">
 	
 	<meta content="width=device-width; initial-scale=0.9;" name="viewport">
 
