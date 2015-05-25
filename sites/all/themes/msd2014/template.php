c5f64bfb46968f917654700154a38b825b135819
Patrick P. Paevere <patrick@paevere.com>
2015-05-25 15:47:47 +1000

    webforms module. better forms.

--- sites/all/themes/msd2014/template.php
+++ sites/all/themes/msd2014/template.php
@@ -8,6 +8,9 @@ function msd2014_preprocess_html(&$vars) {
     drupal_add_css('https://brand.unimelb.edu.au/web-templates/1-1-0/css/complete.css', 'external');
     drupal_add_css("https://brand.unimelb.edu.au/global-header/css/style.css", 'external');
 
+    drupal_add_css('https://uom-design-system.s3.amazonaws.com/v1.0/uom.css', 'external');
+    drupal_add_css(path_to_theme() . '/css/uomtemplates-patch.css');
+
     drupal_add_css(path_to_theme() . '/css/msd2014.css');
     drupal_add_css(path_to_theme() . '/css/nav-style.css');
     drupal_add_css(path_to_theme() . '/css/msd2014-extra.css');
@@ -15,9 +18,10 @@ function msd2014_preprocess_html(&$vars) {
     drupal_add_css(path_to_theme() . '/css/jquery-ui.css');
 
     drupal_add_css(path_to_theme() . '/css/desktop.css');
-
     drupal_add_css(path_to_theme() . '/css/mobile.css');
     drupal_add_css(path_to_theme() . '/css/grey.css');
+    drupal_add_css(path_to_theme() . '/css/forms.css');
+    
 
     drupal_add_js('https://brand.unimelb.edu.au/global-header/js/injection.js', 'external');
     drupal_add_js(path_to_theme() . '/js/supersized.core.3.2.1.min.js');
