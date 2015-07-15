53bdb4230e19e51f80a003dcbf2f0a29b0040abb
Patrick P. Paevere <patrick@paevere.com>
2015-07-15 11:32:20 +1000

    clean up the summary tag

--- sites/all/themes/msd2014/templates/node.tpl.php
+++ sites/all/themes/msd2014/templates/node.tpl.php
@@ -11,11 +11,21 @@ $field_images_url = file_create_url($field_images);
 ?>
 
 <?php if ( node_type_get_name($node) == "News" ): 
-		$field_images ? $main_image = $field_images_url : $main_image = $field_listing_image_url;
+    $field_images ? $main_image = $field_images_url : $main_image = $field_listing_image_url;
+    $field_summary = field_view_field( 'node', $node, 'body', array(
+            'label' => 'hidden', 
+            'type' => 'text_summary_or_trimmed', 
+            'settings' => array()
+          ));
+    // give it a big clean out 
+    $summary = html_entity_decode(preg_replace("/&nbsp;/i", " ", htmlentities(strip_tags($field_summary[0]['#markup']))));
+    $summary = preg_replace('/"/', "'", $summary);
+
     $inline_script = ''
     .'<meta property="og:type"               content="article" /> '
     .'<meta property="og:title"              content="'. drupal_get_title() .'" />'
-    .'<meta property="og:image"              content="'. $main_image .'" />';
+    .'<meta property="og:image"              content="'. $main_image .'" />'
+    .'<meta property="og:description"        content="'. $summary .'" />';
     $element = array(
       '#type' => 'markup',
       '#markup' => $inline_script,
@@ -24,8 +34,6 @@ $field_images_url = file_create_url($field_images);
     endif;
 ?>
 
-
-
 <div class="content-white-wrap"><!--white wrap start-->
 
 <div class="content-left-wrap">
@@ -33,45 +41,44 @@ $field_images_url = file_create_url($field_images);
 <?php 
 $breadcrumb = drupal_get_breadcrumb();
 if ($breadcrumb): ?>
-	<div class="thecrumb">
-		<div id="breadcrumb"><?php print theme('breadcrumb', array('breadcrumb'=>$breadcrumb)); ?></div>
-	</div>
+  <div class="thecrumb">
+    <div id="breadcrumb"><?php print theme('breadcrumb', array('breadcrumb'=>$breadcrumb)); ?></div>
+  </div>
 <?php endif; ?>
   
   <?php if ($title): ?>
       <h1><?php print $title; ?></h1>
   <?php endif; ?>
-
   <?php print render($content); ?>
   
 </div>
 
 <div class="content-right-wrap">
-	
-	<?php
-	$block = module_invoke('sharethis_extras', 'block_view', 'sharethis_extras_block');
-	print render($block['content']);
-	?>
-	
-	<?
-	$item = field_get_items('node', $node, 'field_right_column');
-	if(is_array($item)) {
-	
-		$field_right_column = field_view_value('node', $node, 'field_right_column', $item[0]);
-		if($field_right_column && is_array($field_right_column)) { ?>
-			<div class='right-column'>
-				<? print render($field_right_column); ?>
-			</div>
-	    <? } ?>
+  
+  <?php
+  $block = module_invoke('sharethis_extras', 'block_view', 'sharethis_extras_block');
+  print render($block['content']);
+  ?>
+  
+  <?
+  $item = field_get_items('node', $node, 'field_right_column');
+  if(is_array($item)) {
+  
+    $field_right_column = field_view_value('node', $node, 'field_right_column', $item[0]);
+    if($field_right_column && is_array($field_right_column)) { ?>
+      <div class='right-column'>
+        <? print render($field_right_column); ?>
+      </div>
+      <? } ?>
     
     <? } ?>
     
-	<? if(isset($rightcolumn) && is_array($rightcolumn)) { ?>
-	    <div class='right-column'>
-	    <? print render($rightcolumn); ?>
-	    </div>
+  <? if(isset($rightcolumn) && is_array($rightcolumn)) { ?>
+      <div class='right-column'>
+      <? print render($rightcolumn); ?>
+      </div>
     <? } ?>
-	
+  
 </div>
 <div style="clear:both"></div>
   
