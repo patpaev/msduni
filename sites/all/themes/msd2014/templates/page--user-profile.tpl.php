6e19aa26f9a318d835760d76a21f5f2f2b6fb610
Patrick P. Paevere <patrick@paevere.com>
2016-07-19 10:42:51 +1000

    update hubs and instagram icon

--- sites/all/themes/msd2014/templates/page--user-profile.tpl.php
+++ sites/all/themes/msd2014/templates/page--user-profile.tpl.php
@@ -21,7 +21,7 @@ jQuery(function($){
 	});
 });
 </script>
-  	
+
 	<?php if ($logged_in) : ?>
 		<div class="tabs">
 			<?php print "<ul>" . drupal_render($tabs['#primary']) . "</ul>"; ?>
@@ -33,7 +33,7 @@ jQuery(function($){
 
 
   <div class="wrapper-main">
-  
+
   <div class="main-header">
     <a href="/">
       <svg width="528" height="150" viewBox="0 0 528 150" aria-labelledby="aria-uom-title" role="img">
@@ -41,63 +41,63 @@ jQuery(function($){
         <image xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/<? echo path_to_theme(); ?>/images/msd-full-co-brand-opt-comp.svg" src="<? echo path_to_theme(); ?>/images/msd-full-co-brand.png" width="528" height="150" alt="Melbourne School of Design" preserveAspectRatio="xMaxYMin meet"></image>
       </svg>
     </a>
-      
+
     <div class="navigation">
 	    <div id='cssmenu'>
 		<? print render($main_menu_expanded); ?>
   	    </div>
     </div>
-  
+
     <div style="clear:both"></div>
   </div>
-  
-   <div id="mobile-toggle" style="display:none"> 
+
+   <div id="mobile-toggle" style="display:none">
   <a href="#" id="toggle-menu">
-    <img id="changer" src="/sites/all/themes/msd2014/images/mob-toggle.png" width="85" height="85" onclick="changeImage(this)" />  
+    <img id="changer" src="/sites/all/themes/msd2014/images/mob-toggle.png" width="85" height="85" onclick="changeImage(this)" />
   </a>
   </div>
-  
-  	<?php if ($logged_in) : ?>		
+
+  	<?php if ($logged_in) : ?>
      	<?php print $messages; ?>
 	<?php endif; ?>
- 
+
   <div class="content-white-wrap"><!--white wrap start-->
 
 <div class="content-left-wrap">
 
-<?php 
+<?php
 $breadcrumb = drupal_get_breadcrumb();
 if ($breadcrumb): ?>
 	<div class="thecrumb">
 		<div id="breadcrumb"><?php print theme('breadcrumb', array('breadcrumb'=>$breadcrumb)); ?></div>
 	</div>
 <?php endif; ?>
-  
+
   <?php if ($title): ?>
       <h1><?php print $title; ?></h1>
   <?php endif; ?>
 
-  <?php 
+  <?php
   $user_profile = $page['content']['system_main'];
   ?>
-  
+
   <?php print render($user_profile["field_image"]); ?>
-  
+
   <?php print render($user_profile["field_biography"]); ?>
-  
+
   <?php print render($user_profile["field_related_web_pages"]); ?>
-  
+
   <?php print render($user_profile["field_featured_media_releases"]); ?>
-  
+
   <?php if(array_key_exists("field_find_an_expert", $user_profile)) { ?>
   	<br/>
     For more details visit their <a href='<? print render($user_profile["field_find_an_expert"]); ?>' target='_blank'>Find An Expert profile</a>
   <?php } ?>
-  
+
 </div>
 
 <div class="content-right-wrap">
-	
+
 	<div class='right-column user-profile'>
        <div class="side-module">
 			<h2><?php print $title; ?></h2>
@@ -110,31 +110,31 @@ if ($breadcrumb): ?>
 			     <img src='/sites/default/files/silhouette-150x141px.jpg' />
 			   <?php } ?>
                </p>
-               
+
                <p><strong>Position Title</strong>:<br />
                <?php print render($user_profile["field_position_title"]); ?></p>
-               
+
                <? if(array_key_exists("field_discipline", $user_profile)) { ?>
                  <p>
 	             	<strong>Discipline:</strong><br/>
 	             	<? print render($user_profile["field_discipline"]); ?>
                  </p>
                <? } ?>
-               
+
                <? if(array_key_exists("field_institute_center", $user_profile)) { ?>
                  <p>
-               	 	<strong>Research Centre/Institute:</strong><br />
+               	 	<strong>Research Hub/Centre/Institute:</strong><br />
                	 	<? print render($user_profile["field_institute_center"]); ?>
                  </p>
 			   <? } ?>
-			   
+
                <? if(array_key_exists("field_research_direction", $user_profile)) { ?>
                	 <p>
                	 	<strong>Research Direction:</strong><br />
                	 	<? print render($user_profile["field_research_direction"]); ?>
                	 </p>
                <? } ?>
-               
+
                <p><strong>Contact Details:</strong></p>
                <p><img alt="email icon" class="icon-email middle" src="/sites/default/files/emailIcon.gif" /> <?php print render($user_profile["field_email"]); ?></p>
                <p><img alt="phone icon" class="icon-phone middle" src="/sites/default/files/phoneIcon.gif" /> +61 3 <?php print render($user_profile["field_telephone"]); ?></p>
@@ -147,11 +147,11 @@ if ($breadcrumb): ?>
 
 </div>
 <div style="clear:both"></div>
-  
+
   <div class="backtop"><a href="#">Back to top</a></div>
-  
+
   </div><!--white wrap end-->
-  
+
   </div>
   <div style="clear:both"></div>
 
